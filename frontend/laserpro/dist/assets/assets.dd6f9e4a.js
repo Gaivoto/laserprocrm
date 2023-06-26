f204dc2edfe8c83793f86e6e5f9fd883e66ad058
LeonardoFranco <leo289810@gmail.com>
2023-06-26 23:50:30 +0100

    build test 2

---
+++ frontend/laserpro/dist/assets/assets.dd6f9e4a.js
@@ -0,0 +1,1 @@
+import{B as n,a3 as r}from"./index.3c1e3b15.js";const a=s=>{const t=s.substring(s.lastIndexOf("."),s.length),e=n().mode=="dark"?`${s.substring(0,s.lastIndexOf("."))}-dark`:s.substring(0,s.lastIndexOf("."));return`/media/illustrations/${r.value}/${e}${t}`},d=s=>"/"+s;export{a,d as g};
